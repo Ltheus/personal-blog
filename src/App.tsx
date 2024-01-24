8e29df0916850d98aeb88d7b7bf622ba492b3b7a
MatheusGLopes <matheus.lopes@skyone.solutions>
2024-01-24 16:04:04 -0300

    Added Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-// import { Typography } from "@material-ui/core";
 import "./App.css";
-import { PokeAPICard } from "./components/PokeAPICard/PokeAPICard";
+import { Layout } from "./components/Layout/Layout";
+import { Grid, Typography, makeStyles } from "@material-ui/core";
+// import { PokeAPICard } from "./components/PokeAPICard/PokeAPICard";
 
-//!rafc -> const component snippet 
+//!rafc -> const component snippet
+
+const useStyles = makeStyles({
+  text : {
+    background: "red"
+  }
+})
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div>
-      <PokeAPICard/>
+      {/* <PokeAPICard/> */}
+      <Layout>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Typography className={classes.text}>Hello World</Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography>Hello World</Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography>Hello World</Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography>Hello World</Typography>
+          </Grid>
+        </Grid>
+      </Layout>
     </div>
   );
 }
