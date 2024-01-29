4886483310204e1bdd0935e49131768f3402bab8
MatheusGLopes <matheus.lopes@skyone.solutions>
2024-01-29 16:25:44 -0300

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Layout } from "./components/Layout/Layout";
+import { Route, Switch } from "react-router-dom";
+import Work from "./pages/Work";
 
 //!rafc -> const component snippet
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Layout>
-              <Home />
-            </Layout>
-          </Route>
-          <Route path="/about">
-            <Layout>
-              <About />
-            </Layout>
-          </Route>
-          {/*<Route path="/work">
-            <Work />
-          </Route> */}
-        </Switch>
-      </Router>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/work">
+        <Work />
+      </Route>
+    </Switch>
   );
 }
 
