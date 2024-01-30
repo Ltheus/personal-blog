dcfc030f2e348b4b69e82f1f41e32010921e25d2
MatheusGLopes <matheus.lopes@skyone.solutions>
2024-01-30 17:09:15 -0300

    Added socials

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -11,6 +11,7 @@ import {
 import { Person, Work } from "@material-ui/icons";
 import { useHistory } from "react-router-dom";
 import picture from "../../assets/images/avatar_picture.jpg";
+import { Instagram, GitHub, LinkedIn } from "@material-ui/icons";
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => {
@@ -24,9 +25,16 @@ const useStyles = makeStyles((theme) => {
     },
     drawer: {
       width: drawerWidth,
+      height: "100%",
     },
     drawerPaper: {
       width: drawerWidth,
+      height: "100%",
+      overflowX: "hidden",
+
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
     },
     avatarContainer: {
       width: drawerWidth,
@@ -38,7 +46,11 @@ const useStyles = makeStyles((theme) => {
     avatar: {
       width: "5rem",
       height: "5rem",
-      filter: `drop-shadow(2px 2px 2px ${theme.palette.text.primary})`
+      filter: `drop-shadow(2px 2px 2px ${theme.palette.text.primary})`,
+    },
+    contactTitle: {
+      margin: "0 1rem",
+      padding: 0,
     },
   };
 });
@@ -60,6 +72,24 @@ export const Layout = ({ children }: any) => {
     },
   ];
 
+  const contactItems = [
+    {
+      text: "Instagram",
+      icon: <Instagram />,
+      path: "https://www.instagram.com/gltheus?igsh=MXUwd2QzMDNlajY0eA==",
+    },
+    {
+      text: "GitHub",
+      icon: <GitHub />,
+      path: "https://github.com/Ltheus",
+    },
+    {
+      text: "LinkedIn",
+      icon: <LinkedIn />,
+      path: "https://www.linkedin.com/in/matheus-lopes-74b172265",
+    },
+  ];
+
   return (
     <div className={classes.root}>
       <Drawer
@@ -68,24 +98,49 @@ export const Layout = ({ children }: any) => {
         className={classes.drawer}
         classes={{ paper: classes.drawerPaper }}
       >
-        <Container className={classes.avatarContainer}>
-          <Avatar alt="" src={picture} className={classes.avatar} />
-        </Container>
-        <List>
-          {menuItems.map((item) => (
-            <ListItem
-              button
-              divider
-              key={item.text}
-              onClick={() => {
-                history.push(item.path);
-              }}
-            >
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.text} />
+        <div>
+          <Container className={classes.avatarContainer}>
+            <Avatar alt="" src={picture} className={classes.avatar} />
+          </Container>
+          <List>
+            {menuItems.map((item) => (
+              <ListItem
+                button
+                divider
+                key={item.text}
+                onClick={() => {
+                  history.push(item.path);
+                }}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+        <div>
+          <List>
+            <ListItem component="p" className={classes.contactTitle}>
+              <ListItemText
+                primary="Contact"
+                primaryTypographyProps={{ variant: "h5" }}
+              />
             </ListItem>
-          ))}
-        </List>
+            {contactItems.map((item) => (
+              <ListItem
+                button
+                component="a"
+                href={item.path}
+                target="_blank"
+                divider={item.text !== "LinkedIn" && true}
+                key={item.text}
+              >
+                <ListItemIcon>{item.icon}</ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </Drawer>
       <div className={classes.page}>{children}</div>
     </div>
