c0c5a60ac9463af9a1cd871dbcefa55590ba9dbe
MatheusGLopes <matheus.lopes@skyone.solutions>
2024-02-01 12:18:51 -0300

    Moved classes to a separate file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,43 +4,11 @@ import {
   ImageListItem,
   Paper,
   Typography,
-  makeStyles,
 } from "@material-ui/core";
 import { InterestsCard } from "../components/InterestsCard/InterestsCard";
 import { WorkCard } from "../components/WorkCard/WorkCard";
 import personalPicture from "../assets/images/me_skyone.jpg";
-
-const useStyles = makeStyles((theme) => {
-  return {
-    page: {
-      color: theme?.palette?.text?.primary,
-      margin: "1rem 4rem",
-      overflowY: "visible",
-    },
-    personalImageContainer: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    fontGradientEffect: {
-      background: `linear-gradient(120deg, ${theme?.palette?.text?.primary}, ${theme?.palette?.text?.secondary})`,
-      backgroundClip: "text",
-      WebkitBackgroundClip: "text",
-      WebkitTextFillColor: "transparent",
-    },
-    titleContainer: {
-      padding: "1rem 1rem",
-      display: "flex",
-      justifyContent: "center",
-      gap: "1.5rem",
-    },
-    infoText: {
-      padding: 0,
-      paddingBottom: "1rem",
-      margin: 0,
-    },
-  };
-});
+import { useStyles } from "./HomeStyles";
 
 export default function Home() {
   const classes = useStyles();
