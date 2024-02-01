81e16acbffe194541e83f86f65246527afdc2d27
MatheusGLopes <matheus.lopes@skyone.solutions>
2024-01-31 22:39:23 -0300

    Updated root style and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
 import "./App.css";
 import Home from "./pages/Home";
-import { Route, Switch } from "react-router-dom";
-import APIPage from "./pages/APIPage";
 
 function App() {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/api">
-        <APIPage />
-      </Route>
-    </Switch>
-  );
+  return <Home />;
 }
 
 export default App;
